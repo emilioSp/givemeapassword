f9a4513ffb3f742b68b91ec20199de419a26370a
espatola <emilio.spatola@gmail.com>
2019-09-08 12:13:27 +0200

    Add helmet

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const Koa = require('koa');
+const helmet = require("koa-helmet");
 const removeTrailingSlashes = require('koa-remove-trailing-slashes');
 const router = require('./routes/router');
 
 const app = new Koa();
+app.use(helmet());
 app.use(removeTrailingSlashes());
 app.use(router.routes());
 app.use(router.allowedMethods());
