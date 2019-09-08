a334c524506ba9eaa1588424f0b31239d7fe67b2
espatola <emilio.spatola@gmail.com>
2019-09-08 13:48:26 +0200

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ docker build --no-cache -t espatola/givemeapassword -f Dockerfile . \
 docker pull espatola/givemeapassword \
 && (docker stop givemeapassword || true)\
 && (docker rm givemeapassword || true)\
-&& docker run --detach --restart=unless-stopped --hostname \$(hostname) -p 80:80 -it --name givemeapassword espatola/givemeapassword:latest"
+&& docker run --detach --restart=unless-stopped --hostname \$(hostname) -p 3000:3000 -it --name givemeapassword espatola/givemeapassword:latest"
