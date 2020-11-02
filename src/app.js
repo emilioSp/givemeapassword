bcbf6773db77fddbd76859fafa2d0724a7e47d74
emilioSp <emilio.spatola@gmail.com>
2020-11-02 13:13:41 +0100

    Add koa-compress

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const Koa = require('koa');
 const helmet = require("koa-helmet");
 const serve = require('koa-static');
+const compress = require('koa-compress');
 const removeTrailingSlashes = require('koa-remove-trailing-slashes');
 const router = require('./routes/router');
 
 const app = new Koa();
 app.use(helmet());
 app.use(removeTrailingSlashes());
+app.use(compress());
 app.use(serve('src/public'));
 app.use(router.routes());
 app.use(router.allowedMethods());
